75366f6b1224c15c594b184b57fca57c0ac9708f
DiegoDiaz1 <diegojay-16@hotmail.com>
2017-09-20 12:53:04 -0600

    Add files via upload
    Prueba de Huffman falta implementarlo al DLL y hacer que obtenga el BitArray para descomprimir

---
+++ Huffman/Huffman/HuffmanTree.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Huffman
+{
+    class HuffmanTree
+    {
+        private List<Node> nodes = new List<Node>();
+        public Node Root { get; set; }
+        public Dictionary<byte, int> Frequencies = new Dictionary<byte, int>();
+
+        public void Build(byte[] source)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Frequencies.ContainsKey((source[i])))
+                {
+                    Frequencies.Add(source[i], 0);
+                }
+
+                Frequencies[source[i]]++;
+            }
+
+            foreach (KeyValuePair<byte, int> symbol in Frequencies)
+            {
+                nodes.Add(new Node() { Symbol = symbol.Key, Frequency = symbol.Value });
+            }
+
+            while (nodes.Count > 1)
+            {
+                List<Node> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList<Node>();
+
+                if (orderedNodes.Count >= 2)
+                {
+                    // Take first two items
+                    List<Node> taken = orderedNodes.Take(2).ToList<Node>();
+                    char simbolo = '*';
+                    // Create a parent node by combining the frequencies
+                    Node parent = new Node()
+                    {
+                        Symbol = Convert.ToByte(simbolo) ,
+                        Frequency = taken[0].Frequency + taken[1].Frequency,
+                        Left = taken[0],
+                        Right = taken[1]
+                    };
+
+                    nodes.Remove(taken[0]);
+                    nodes.Remove(taken[1]);
+                    nodes.Add(parent);
+                }
+
+                this.Root = nodes.FirstOrDefault();
+
+            }
+
+        }
+
+        public BitArray Encode(byte[] source)
+        {
+            List<bool> encodedSource = new List<bool>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                List<bool> encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                encodedSource.AddRange(encodedSymbol);
+            }
+
+            BitArray bits = new BitArray(encodedSource.ToArray());
+
+            return bits;
+        }
+
+        public byte[] Decode(BitArray bits)
+        {
+            Node current = this.Root;
+            byte[] decoded = new byte[bits.Length];
+            int i = 0;
+
+            foreach (bool bit in bits)
+            {
+                if (bit)
+                {
+                    if (current.Right != null)
+                    {
+                        current = current.Right;
+                    }
+                }
+                else
+                {
+                    if (current.Left != null)
+                    {
+                        current = current.Left;
+                    }
+                }
+
+                if (IsLeaf(current))
+                {
+
+                        decoded[i] += current.Symbol;
+                        current = this.Root;
+                        i++;
+                    
+                }
+            }
+
+            return decoded;
+        }
+
+        public bool IsLeaf(Node node)
+        {
+            return (node.Left == null && node.Right == null);
+        }
+    }
+}
---
+++ Huffman/Huffman/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace Huffman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HuffmanTree huffmanTree = new HuffmanTree();
+            string ruta2 = "C:\\Users\\Diego Diaz\\Desktop\\Descompresion2.txt";
+            Console.WriteLine("Escribir la ruta del archivo:");
+            string ruta = Console.ReadLine();
+            byte[] mensaje = File.ReadAllBytes(ruta);
+            if (File.Exists(ruta))
+            {
+                huffmanTree.Build(mensaje);
+                BitArray encoded = huffmanTree.Encode(mensaje);
+
+                Console.Write("Encoded: ");
+                byte[] bytes = new byte[encoded.Length / 8 + (encoded.Length % 8 == 0 ? 0 : 1)];
+                encoded.CopyTo(bytes, 0);
+                File.WriteAllBytes(@"C:\Users\Diego Diaz\Desktop\Compressed.txt", bytes);
+                Console.WriteLine();
+
+                byte[] decoded = huffmanTree.Decode(encoded);
+                File.Create(ruta2).Dispose();
+                using (StreamWriter textWriter = new StreamWriter(ruta2, true))
+                {
+                    for (int i = 0; i < decoded.Length; i++)
+                    {
+                        textWriter.Write(System.Text.Encoding.ASCII.GetString(new byte[] { decoded[i] }));
+                    }
+                }
+
+                    Console.ReadLine();
+            }
+        }
+    }
+}
