2cc943f8d0480c22c724fdf25581fc83f30ec50f
DiegoDiaz1 <diegojay-16@hotmail.com>
2017-09-19 18:10:39 -0600

    LAb2 V2
    Incluye la descompresion momentanea del RLE

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -14,11 +14,14 @@ namespace Lab02
             //Ingresar la ruta del archivo original
             Console.WriteLine("Escribir la ruta del archivo:");
             string ruta = Console.ReadLine();
+            string ruta2 = "C:\\Users\\Diego Diaz\\Desktop\\Descompresion.txt";
             //Si el archivo existe
             if (File.Exists(ruta)) 
             {
                 byte[] Archivo = File.ReadAllBytes(ruta); //Leer el archivo como un arreglo de bytes.
                 byte auxiliar = Archivo[0]; //Obtengo el contenido del primer byte.
+                string[] Mensaje = new string[Archivo.Length];//Arreglo en donde se Guardan las letras del mensaje;
+                int[] cantidadDeRepeticiones = new int[Archivo.Length];//Arreglo donde se guardan las veces que se repite una letra;
                 int Repeticiones = 0;
                 StringBuilder Salida = new StringBuilder(); //Ire acumulando strings para mandar al archivo de salida
                 for (int n = 0; n < Archivo.Length; n++)
@@ -31,11 +34,34 @@ namespace Lab02
                     {
                         Salida.AppendLine("Repeticiones: " + Repeticiones.ToString() + " Caracter: " + auxiliar);
                         auxiliar = Archivo[n];
+                        Mensaje[n] = System.Text.Encoding.ASCII.GetString(new[] { auxiliar });//obtengo la letra y la guardo en el array;
+                        cantidadDeRepeticiones[n] = Repeticiones;//obtengo las veces que se repito esta letra;
                         Repeticiones = 1;
                     }
+
                 }
                 //Por el diseño del for los datos del último caracter no son escritos, los escribo ahora.
                 Salida.AppendLine("Repeticiones: " + Repeticiones.ToString() + " Caracter: " + auxiliar);
+
+                File.Create(ruta2).Close();//Creo un archivo si no existe eh inmediatamente lo cierro
+
+                //Escribo el mensaje decodificado
+                using (StreamWriter textWriter = new StreamWriter(ruta2, true))
+                {
+                    int j;
+                    for (int i = 0; i < Mensaje.Length; i++)
+                    {
+                        int rep = cantidadDeRepeticiones[i];
+                        j = 0;
+                        while (j < rep)
+                        {
+                            textWriter.Write(Mensaje[i]);//Se Ecribe la letra en el texto 
+                            j++;
+                        }
+                    }
+                    textWriter.Close();//Cierro el StreamWriter
+                }
+
                 Console.WriteLine(Salida); //Escribir todos los string que acumulé.
                 Console.ReadLine();
             }
